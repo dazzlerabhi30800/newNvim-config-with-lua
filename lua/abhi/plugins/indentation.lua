e39f388c92ceb9446f9ddb41fa1c8855533dbca8
abhishekchoudharywcs <abhishek.dev@webconsultancyservices.in>
2024-06-15 13:40:06 +0530

    added new config for indentation

--- lua/abhi/plugins/indentation.lua
+++ lua/abhi/plugins/indentation.lua
@@ -3,33 +3,32 @@ return {
 		"lukas-reineke/indent-blankline.nvim",
 		event = { "BufReadPre", "BufNewFile" },
 		main = "ibl",
-		opts = {
-			indent = { char = "┊" },
-		},
-		-- config = function()
-		-- 	require("ibl").setup({
-		-- 		indent = {
-		-- 			-- char = "|",
-		-- 			char = "▏",
-		--
-		-- 			-- highlight = { "Whitespace", "CursorColumn", "Function", "Label" },
-		-- 			highlight = { "Whitespace", "CursorColumn" },
-		-- 		},
-		-- 		-- whitespace = {
-		-- 		-- 	highlight = "IblWhitespace",
-		-- 		-- 	remove_blankline_trail = true,
-		-- 		-- },
-		-- 		-- scope = {
-		-- 		-- 	include = {
-		-- 		-- 		node_type = {
-		-- 		-- 			lua = {
-		-- 		-- 				"*",
-		-- 		-- 			},
-		-- 		-- 			css = { "*" },
-		-- 		-- 		},
-		-- 		-- 	},
-		-- 		-- },
-		-- 	})
-		-- end,
+		opts = {},
+		-- opts = {
+		-- 	indent = { char = "┊" },
+		-- },
+		config = function()
+			require("ibl").setup({
+				indent = {
+					char = "┊",
+					highlight = { "Whitespace", "CursorColumn", "Function", "Label" },
+					-- highlight = { "Whitespace", "CursorColumn" },
+				},
+				-- whitespace = {
+				-- 	highlight = "IblWhitespace",
+				-- 	remove_blankline_trail = true,
+				-- },
+				-- scope = {
+				-- 	include = {
+				-- 		node_type = {
+				-- 			lua = {
+				-- 				"*",
+				-- 			},
+				-- 			css = { "*" },
+				-- 		},
+				-- 	},
+				-- },
+			})
+		end,
 	},
 }
